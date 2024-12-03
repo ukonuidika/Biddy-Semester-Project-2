86062aa27543307d21634a745d47b82cec041898
ukonuidika <120014847+ukonuidika@users.noreply.github.com>
2024-12-03 16:16:31 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./auth//*.html",
+    "./post//*.html",
+    "./profile//*.html",
+    "./src//*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
